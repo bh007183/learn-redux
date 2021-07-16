3b35d9fc76192fe964fda4fbf351bc774626d1e7
Ben Hopkins <bjhops17@gmail.com>
2021-07-16 15:21:15 -0700

    added new promis

--- Practice/Promise-Async/server.js
+++ Practice/Promise-Async/server.js
@@ -3,61 +3,31 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8080
 
-// Promis //
-// const catsError = {
-//     message: "This cat is an error, there has been a mistake with your query. No such user",
-//     status: 401,
-//     response: "error"
-// }
-// const catsSuccess = {
-//     message: "This is the most fluffy cat of all timer",
-//     status: 200,
-//     response: "Success"
-// }
-
-
-// function getCat(obj){
-//     return new Promise((resolve, reject) => {
-//         const keyArr = Object.values(obj)
-
-//         switch (keyArr.includes("Success")) {
-//             case false:
-//                 reject(obj)
-                
-//                 break;
-//             case true:
-//                 resolve(obj)
-                
-//                 break;
-        
-//             default:
-//                 break;
-//         }
-
-
-//     })
-// }
-
-// getCat(catsError).then(data => console.log(data)).catch(err => console.log(err))
-
-let arrArr = [1,4,5,6,7,8,9,0,4,3,6,5,7,8,"g"]
-
-async function getCat(obj){ // this will not settle undile data is reseaved
-
-    const data = await arrArr.filter(item => item === "g")
-    console.log(data)
-    console.log(1)
-   
-
-
+function printDog(){
+    console.log("Dog")
 }
 
-getCat()
 
 
-app.listen(PORT, function(){
-    console.log(PORT)
-})
+function fetch(res){
+    
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(res){
+                    resolve(res)
+                }else{
+                    reject(res)
+                }  
+                
+            }, 4000);
+            
+        })
+}
+
+fetch(true).then(res => console.log(res + " success")).catch(err => console.log(err + " error"))
+
+
+
 
 
 
